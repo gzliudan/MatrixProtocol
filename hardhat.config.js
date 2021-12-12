03dab6d89f9cd025062670fa964a47fb423e37a9
Dan Liu <liudaniel@qq.com>
2021-12-12 22:00:25 +0800

    setup compiler and network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,40 @@
 /* global task */
-require('dotenv').config();
 
 require('@nomiclabs/hardhat-etherscan');
 require('@nomiclabs/hardhat-waffle');
-require('hardhat-gas-reporter');
-require('solidity-coverage');
 require('hardhat-contract-sizer');
+require('dotenv').config();
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+const enableGasReport = process.env.REPORT_GAS ? true : false;
+
+if (enableGasReport) {
+  require('hardhat-gas-reporter');
+}
+
+if (process.env.COVERAGE) {
+  require('solidity-coverage');
+}
+
+const ether = (n) => `${n}${'0'.repeat(18)}`;
+
+const MUMBAI_RPC_LIST = [
+  'https://rpc-mumbai.matic.today',
+  'https://matic-mumbai.chainstacklabs.com',
+  'https://rpc-mumbai.maticvigil.com',
+  'https://matic-testnet-archive-rpc.bwarelabs.com',
+];
+
+const POLYGON_RPC_LIST = [
+  'https://polygon-rpc.com',
+  'https://rpc-mainnet.matic.network',
+  'https://matic-mainnet.chainstacklabs.com',
+  'https://rpc-mainnet.maticvigil.com',
+  'https://rpc-mainnet.matic.quiknode.11p',
+  'https://matic-mainnet-full-rpc.bwarel11ab',
+];
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -24,24 +53,91 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.13',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 250,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {
+      blockGasLimit: 2e7,
+      allowUnlimitedContractSize: true,
+      accounts: {
+        count: 100,
+        accountsBalance: ether(1000000),
+      },
+    },
+    local: {
+      url: 'http://localhost:8545',
+    },
+    bsc_testnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      chainId: 97,
+    },
+    bsc: {
+      url: `https://bsc-dataseed.binance.org`,
+      chainId: 56,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_LIST[1],
+      chainId: 80001,
+    },
+    polygon: {
+      url: POLYGON_RPC_LIST[0],
+      chainId: 137,
+    },
     ropsten: {
-      url: process.env.ROPSTEN_URL || '',
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      chainId: 3,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      chainId: 4,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      chainId: 42,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      chainId: 1,
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: enableGasReport,
     currency: 'USD',
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      mainnet: ETHERSCAN_API_KEY,
+      ropsten: ETHERSCAN_API_KEY,
+      rinkeby: ETHERSCAN_API_KEY,
+      kovan: ETHERSCAN_API_KEY,
+      // binance smart chain
+      bsc: 'YOUR_BSCSCAN_API_KEY',
+      bscTestnet: 'YOUR_BSCSCAN_API_KEY',
+      // polygon
+      polygon: POLYGONSCAN_API_KEY,
+      polygonMumbai: POLYGONSCAN_API_KEY,
+    },
   },
   contractSizer: {
     alphaSort: true,
     disambiguatePaths: true,
     runOnCompile: false,
     strict: false,
+  },
+  mocha: {
+    timeout: 20000,
   },
 };
