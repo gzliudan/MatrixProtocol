d9a5aa234d9389879113af424a3236a9a1bc58ae
Dan Liu <liudaniel@qq.com>
2021-12-13 00:21:57 +0800

    add interface IMatrixToken

---
+++ contracts/interfaces/IMatrixToken.sol
@@ -0,0 +1,188 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+// ==================== External Imports ====================
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title IMatrixToken
+ */
+interface IMatrixToken is IERC20 {
+    // ==================== Enums ====================
+
+    enum ModuleState {
+        NONE,
+        PENDING,
+        INITIALIZED
+    }
+
+    // ==================== Structs ====================
+
+    /**
+     * @dev The base definition of a MatrixToken Position
+     *
+     * @param unit             Each unit is the # of components per 10^18 of a MatrixToken
+     * @param module           If not in default state, the address of associated module
+     * @param component        Address of token in the Position
+     * @param positionState    Position ENUM. Default is 0; External is 1
+     * @param data             Arbitrary data
+     */
+    struct Position {
+        int256 unit;
+        address module;
+        address component;
+        uint8 positionState;
+        bytes data;
+    }
+
+    /**
+     * @dev A struct that stores a component's external position details including virtual unit and any auxiliary data.
+     *
+     * @param virtualUnit    Virtual value of a component's EXTERNAL position.
+     * @param data           Arbitrary data
+     */
+    struct ExternalPosition {
+        int256 virtualUnit;
+        bytes data;
+    }
+
+    /**
+     * @dev A struct that stores a component's cash position details and external positions
+     * This data structure allows O(1) access to a component's cash position units and  virtual units.
+     *
+     * @param virtualUnit                Virtual value of a component's DEFAULT position. Stored as virtual for efficiency updating all units
+     *                                   at once via the position multiplier. Virtual units are achieved by dividing a real value by the positionMultiplier
+     * @param externalPositionModules    Eexternal modules attached to each external position. Each module maps to an external position
+     * @param externalPositions          Mapping of module => ExternalPosition struct for a given component
+     */
+    struct ComponentPosition {
+        int256 virtualUnit;
+        address[] externalPositionModules;
+        mapping(address => ExternalPosition) externalPositions;
+    }
+
+    // ==================== Events ====================
+
+    event Invoke(address indexed target, uint256 indexed value, bytes data, bytes returnValue);
+    event AddModule(address indexed module);
+    event RemoveModule(address indexed module);
+    event InitializeModule(address indexed module);
+    event EditManager(address newManager, address oldManager);
+    event RemovePendingModule(address indexed module);
+    event EditPositionMultiplier(int256 newMultiplier);
+    event AddComponent(address indexed component);
+    event RemoveComponent(address indexed component);
+    event EditDefaultPositionUnit(address indexed component, int256 realUnit);
+    event EditExternalPositionUnit(address indexed component, address indexed positionModule, int256 realUnit);
+    event EditExternalPositionData(address indexed component, address indexed positionModule, bytes data);
+    event AddPositionModule(address indexed component, address indexed positionModule);
+    event RemovePositionModule(address indexed component, address indexed positionModule);
+
+    // ==================== External functions ====================
+
+    function getController() external view returns (address);
+
+    function getManager() external view returns (address);
+
+    function getLocker() external view returns (address);
+
+    function getComponents() external view returns (address[] memory);
+
+    function getModules() external view returns (address[] memory);
+
+    function getModuleState(address module) external view returns (ModuleState);
+
+    function getPositionMultiplier() external view returns (int256);
+
+    function getPositions() external view returns (Position[] memory);
+
+    function getTotalComponentRealUnits(address component) external view returns (int256);
+
+    function getDefaultPositionRealUnit(address component) external view returns (int256);
+
+    function getExternalPositionRealUnit(address component, address positionModule) external view returns (int256);
+
+    function getExternalPositionModules(address component) external view returns (address[] memory);
+
+    function getExternalPositionData(address component, address positionModule) external view returns (bytes memory);
+
+    function isExternalPositionModule(address component, address module) external view returns (bool);
+
+    function isComponent(address component) external view returns (bool);
+
+    function isInitializedModule(address module) external view returns (bool);
+
+    function isPendingModule(address module) external view returns (bool);
+
+    function isLocked() external view returns (bool);
+
+    function setManager(address manager) external;
+
+    function addComponent(address component) external;
+
+    function removeComponent(address component) external;
+
+    function editDefaultPositionUnit(address component, int256 realUnit) external;
+
+    function addExternalPositionModule(address component, address positionModule) external;
+
+    function removeExternalPositionModule(address component, address positionModule) external;
+
+    function editExternalPositionUnit(
+        address component,
+        address positionModule,
+        int256 realUnit
+    ) external;
+
+    function editExternalPositionData(
+        address component,
+        address positionModule,
+        bytes calldata data
+    ) external;
+
+    function invoke(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bytes memory);
+
+    function invokeSafeIncreaseAllowance(
+        address token,
+        address spender,
+        uint256 amount
+    ) external;
+
+    function invokeSafeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) external;
+
+    function invokeExactSafeTransfer(
+        address token,
+        address to,
+        uint256 amount
+    ) external;
+
+    function invokeWrapWETH(address weth, uint256 amount) external;
+
+    function invokeUnwrapWETH(address weth, uint256 amount) external;
+
+    function editPositionMultiplier(int256 newMultiplier) external;
+
+    function mint(address account, uint256 quantity) external;
+
+    function burn(address account, uint256 quantity) external;
+
+    function lock() external;
+
+    function unlock() external;
+
+    function addModule(address module) external;
+
+    function removeModule(address module) external;
+
+    function initializeModule() external;
+}
