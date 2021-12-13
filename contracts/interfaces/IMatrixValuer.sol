fc36fa0f13865889469ada48e2dd39e7a039f144
Dan Liu <liudaniel@qq.com>
2021-12-13 14:56:40 +0800

    add interface IMatrixValuer

---
+++ contracts/interfaces/IMatrixValuer.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+import { IMatrixToken } from "../interfaces/IMatrixToken.sol";
+
+/**
+ * @title IMatrixValuer
+ */
+interface IMatrixValuer {
+    // ==================== External functions ====================
+
+    function calculateMatrixTokenValuation(IMatrixToken matrixToken, address quoteAsset) external view returns (uint256);
+}
