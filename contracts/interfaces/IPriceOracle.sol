90af217c7c3fb31c72d6eac36f1fb3c30aab029b
Dan Liu <liudaniel@qq.com>
2021-12-13 14:57:00 +0800

    add interface IPriceOracle

---
+++ contracts/interfaces/IPriceOracle.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title IPriceOracle
+ */
+interface IPriceOracle {
+    // ==================== Events ====================
+
+    event AddPair(address indexed asset1, address indexed asset2, address indexed oracle);
+    event RemovePair(address indexed asset1, address indexed asset2, address indexed oracle);
+    event EditPair(address indexed asset1, address indexed asset2, address indexed newOracle);
+    event AddAdapter(address indexed adapter);
+    event RemoveAdapter(address indexed adapter);
+    event EditMasterQuoteAsset(address indexed newMasterQuote);
+    event EditSecondQuoteAsset(address indexed newSecondQuote);
+
+    // ==================== External functions ====================
+
+    function getController() external view returns (address);
+
+    function getOracle(address asset1, address asset2) external view returns (address);
+
+    function getMasterQuoteAsset() external view returns (address);
+
+    function getSecondQuoteAsset() external view returns (address);
+
+    function getAdapters() external view returns (address[] memory);
+
+    function getPrice(address asset1, address asset2) external view returns (uint256);
+
+    function addPair(
+        address asset1,
+        address asset2,
+        address oracle
+    ) external;
+
+    function editPair(
+        address asset1,
+        address asset2,
+        address oracle
+    ) external;
+
+    function removePair(address asset1, address asset2) external;
+
+    function addAdapter(address adapter) external;
+
+    function removeAdapter(address adapter) external;
+
+    function editMasterQuoteAsset(address newMasterQuoteAsset) external;
+
+    function editSecondQuoteAsset(address newSecondQuoteAsset) external;
+}
