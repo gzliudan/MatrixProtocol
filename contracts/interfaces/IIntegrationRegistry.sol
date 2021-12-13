07c9dd2bf47b4b1d9713ff7d311752753375076a
Dan Liu <liudaniel@qq.com>
2021-12-13 14:56:22 +0800

    add interface IIntegrationRegistry

---
+++ contracts/interfaces/IIntegrationRegistry.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+/*
+ * @title IIntegrationRegistry
+ */
+interface IIntegrationRegistry {
+    // ==================== Events ====================
+
+    event AddIntegration(address indexed module, address indexed adapter, string integrationName);
+    event RemoveIntegration(address indexed module, address indexed adapter, string integrationName);
+    event EditIntegration(address indexed module, address newAdapter, string integrationName);
+
+    // ==================== External functions ====================
+
+    function getIntegrationAdapter(address module, string memory id) external view returns (address);
+
+    function getIntegrationAdapterWithHash(address module, bytes32 id) external view returns (address);
+
+    function isValidIntegration(address module, string memory id) external view returns (bool);
+
+    function addIntegration(address module, string memory id, address wrapper) external; // prettier-ignore
+
+    function batchAddIntegration(address[] memory modules, string[] memory names, address[] memory adapters) external; // prettier-ignore
+
+    function editIntegration(address module, string memory name, address adapter) external; // prettier-ignore
+
+    function batchEditIntegration(address[] memory modules, string[] memory names, address[] memory adapters) external; // prettier-ignore
+
+    function removeIntegration(address module, string memory name) external;
+}
