cc505f4460b5ce60da0abc189129755b11158b72
Dan Liu <liudaniel@qq.com>
2021-12-13 14:56:03 +0800

    add interface IController

---
+++ contracts/interfaces/IController.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+// ==================== Internal Imports ====================
+
+import { IPriceOracle } from "../interfaces/IPriceOracle.sol";
+import { IMatrixValuer } from "../interfaces/IMatrixValuer.sol";
+import { IIntegrationRegistry } from "../interfaces/IIntegrationRegistry.sol";
+
+/**
+ * @title IController
+ */
+interface IController {
+    // ==================== Events ====================
+
+    event AddFactory(address indexed factory);
+    event RemoveFactory(address indexed factory);
+    event AddFee(address indexed module, uint256 indexed feeType, uint256 feePercentage);
+    event EditFee(address indexed module, uint256 indexed feeType, uint256 feePercentage);
+    event EditFeeRecipient(address newFeeRecipient);
+    event AddModule(address indexed module);
+    event RemoveModule(address indexed module);
+    event AddResource(address indexed resource, uint256 id);
+    event RemoveResource(address indexed resource, uint256 id);
+    event AddMatrix(address indexed matrixToken, address indexed factory);
+    event RemoveMatrix(address indexed matrixToken);
+
+    // ==================== External functions ====================
+
+    function isMatrix(address matrixToken) external view returns (bool);
+
+    function isFactory(address addr) external view returns (bool);
+
+    function isModule(address addr) external view returns (bool);
+
+    function isResource(address addr) external view returns (bool);
+
+    function isSystemContract(address contractAddress) external view returns (bool);
+
+    function getFeeRecipient() external view returns (address);
+
+    function getModuleFee(address module, uint256 feeType) external view returns (uint256);
+
+    function getFactories() external view returns (address[] memory);
+
+    function getModules() external view returns (address[] memory);
+
+    function getResources() external view returns (address[] memory);
+
+    function getResource(uint256 id) external view returns (address);
+
+    function getMatrixs() external view returns (address[] memory);
+
+    function getIntegrationRegistry() external view returns (IIntegrationRegistry);
+
+    function getPriceOracle() external view returns (IPriceOracle);
+
+    function getMatrixValuer() external view returns (IMatrixValuer);
+
+    function initialize(
+        address[] memory factories,
+        address[] memory modules,
+        address[] memory resources,
+        uint256[] memory resourceIds
+    ) external;
+
+    function addMatrix(address matrixToken) external;
+
+    function removeMatrix(address matrixToken) external;
+
+    function addFactory(address factory) external;
+
+    function removeFactory(address factory) external;
+
+    function addModule(address module) external;
+
+    function removeModule(address module) external;
+
+    function addResource(address resource, uint256 id) external;
+
+    function removeResource(uint256 id) external;
+
+    function addFee(
+        address module,
+        uint256 feeType,
+        uint256 newFeePercentage
+    ) external;
+
+    function editFee(
+        address module,
+        uint256 feeType,
+        uint256 newFeePercentage
+    ) external;
+
+    function editFeeRecipient(address newFeeRecipient) external;
+}
