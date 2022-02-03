253b2f01f61c340cb58b22ab3f52d5b14f407ae0
Dan Liu <liudaniel@qq.com>
2022-02-04 00:35:25 +0800

    add contract ChainlinkSerialOracle

---
+++ contracts/protocol/integration/oracles/ChainlinkSerialOracle.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.0;
+
+// ==================== External Imports ====================
+
+import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+// ==================== Internal Imports ====================
+
+import { IOracle } from "../../../interfaces/IOracle.sol";
+
+/**
+ * @title ChainlinkSerialOracle
+ * @author Matrix
+ */
+contract ChainlinkSerialOracle is IOracle {
+    using SafeCast for int256;
+
+    // ==================== Variables ====================
+
+    string internal _name;
+    int256 internal _ratio;
+
+    AggregatorV3Interface internal immutable _priceFeed1;
+    AggregatorV3Interface internal immutable _priceFeed2;
+
+    // ==================== Constructor function ====================
+
+    constructor(
+        string memory name,
+        AggregatorV3Interface priceFeed1,
+        AggregatorV3Interface priceFeed2
+    ) {
+        _name = name;
+        _priceFeed1 = priceFeed1;
+        _priceFeed2 = priceFeed2;
+
+        uint8 decimals = priceFeed1.decimals() + priceFeed2.decimals();
+
+        if (decimals > 18) {
+            _ratio = int256(10)**(decimals - 18);
+        } else if (decimals < 18) {
+            _ratio = -(int256(10)**(18 - decimals));
+        }
+    }
+
+    // ==================== External functions ====================
+
+    function getName() external view returns (string memory) {
+        return _name;
+    }
+
+    function getRatio() external view returns (int256) {
+        return _ratio;
+    }
+
+    function getPriceFeed1() external view returns (AggregatorV3Interface) {
+        return _priceFeed1;
+    }
+
+    function getPriceFeed2() external view returns (AggregatorV3Interface) {
+        return _priceFeed2;
+    }
+
+    /// @dev Returns the latest price, multiplied by 1e18
+    function read() external view returns (uint256 price) {
+        (, int256 price1, , , ) = _priceFeed1.latestRoundData(); // base * decimals1 / middle
+        (, int256 price2, , , ) = _priceFeed2.latestRoundData(); // middle * decimals2 / quote
+
+        price = price1.toUint256() * price2.toUint256(); // base * decimals1 * decimals2 / quote
+
+        int256 ratio = _ratio; // for save gas
+        if (ratio > 0) {
+            price /= ratio.toUint256();
+        } else if (ratio < 0) {
+            price *= (-ratio).toUint256();
+        }
+    }
+}
---
+++ test/protocol/integration/oracles/ChainlinkSerialOracle.test.js
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// ==================== External Imports ====================
+
+const { expect } = require('chai');
+const { BigNumber } = require('hardhat').ethers;
+
+// ==================== Internal Imports ====================
+
+const { deployContract } = require('../../../helpers/deploy');
+const { getSigners, getRandomAddress } = require('../../../helpers/accountUtil');
+const { snapshotBlockchain, revertBlockchain } = require('../../../helpers/evmUtil.js');
+
+describe('contract ChainlinkSerialOracle', async () => {
+  const [owner] = await getSigners();
+  const name = 'BTC/ETH/USD';
+  const price1 = BigNumber.from(1000);
+  const price2 = BigNumber.from(10);
+  const expectedPrice = price1.mul(price2).mul(BigNumber.from(10).pow(18));
+
+  let decimals1;
+  let decimals2;
+  let priceFeedMock1;
+  let priceFeedMock2;
+  let priceFeed1Address;
+  let priceFeed2Address;
+  let oracle;
+
+  let snapshotId;
+  before(async () => {
+    snapshotId = await snapshotBlockchain();
+  });
+
+  after(async () => {
+    await revertBlockchain(snapshotId);
+  });
+
+  describe('constructor', async () => {
+    let snapshotId;
+    beforeEach(async () => {
+      snapshotId = await snapshotBlockchain();
+
+      decimals1 = 18;
+      priceFeedMock1 = await deployContract('ChainlinkPriceFeedMock', [price1, decimals1], owner);
+      priceFeed1Address = priceFeedMock1.address;
+
+      decimals2 = 18;
+      priceFeedMock2 = await deployContract('ChainlinkPriceFeedMock', [price2, decimals2], owner);
+      priceFeed2Address = priceFeedMock2.address;
+    });
+
+    afterEach(async () => {
+      await revertBlockchain(snapshotId);
+    });
+
+    async function createOracle() {
+      return deployContract('ChainlinkSerialOracle', [name, priceFeed1Address, priceFeed2Address], owner);
+    }
+
+    it('should reverted when priceFeed1 is invalid', async () => {
+      priceFeed1Address = await getRandomAddress();
+      await expect(createOracle()).reverted;
+    });
+
+    it('should reverted when priceFeed1 is invalid', async () => {
+      priceFeed2Address = await getRandomAddress();
+      await expect(createOracle()).reverted;
+    });
+
+    it('should return the correct name', async () => {
+      oracle = await createOracle();
+      const result = await oracle.getName();
+      expect(result).eq(name);
+    });
+
+    it('should return the correct getPriceFeed1', async () => {
+      oracle = await createOracle();
+      const priceFeed1 = await oracle.getPriceFeed1();
+      expect(priceFeed1).eq(priceFeed1Address);
+    });
+
+    it('should return the correct getPriceFeed2', async () => {
+      oracle = await createOracle();
+      const priceFeed2 = await oracle.getPriceFeed2();
+      expect(priceFeed2).eq(priceFeed2Address);
+    });
+  });
+
+  describe('read', async () => {
+    let snapshotId;
+    beforeEach(async () => {
+      snapshotId = await snapshotBlockchain();
+    });
+
+    afterEach(async () => {
+      await revertBlockchain(snapshotId);
+    });
+
+    async function read() {
+      priceFeedMock1 = await deployContract('ChainlinkPriceFeedMock', [price1, decimals1], owner);
+      priceFeed1Address = priceFeedMock1.address;
+      priceFeedMock2 = await deployContract('ChainlinkPriceFeedMock', [price2, decimals2], owner);
+      priceFeed2Address = priceFeedMock2.address;
+      oracle = await deployContract('ChainlinkSerialOracle', [name, priceFeed1Address, priceFeed2Address], owner);
+      return oracle.read();
+    }
+
+    for (let i = 0; i <= 18; i++) {
+      for (let j = 0; j <= 18; j++) {
+        it(`when decimals1 is ${i} and decimals2 is ${j}`, async () => {
+          decimals1 = i;
+          decimals2 = j;
+          const result = await read();
+          expect(result).eq(expectedPrice);
+        });
+      }
+    }
+  });
+});
