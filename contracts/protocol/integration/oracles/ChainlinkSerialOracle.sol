47cc64a2f6be663dff752ca695d69e30e0c4336c
Daniel Liu <liudaniel@qq.com>
2024-04-04 12:00:47 +0800

    upgrade @chainlink/contracts to v1.0.0

--- contracts/protocol/integration/oracles/ChainlinkSerialOracle.sol
+++ contracts/protocol/integration/oracles/ChainlinkSerialOracle.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 // ==================== External Imports ====================
 
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 // ==================== Internal Imports ====================
 
